1a8626b4a62703d74e1861e333643e9a4a18249c
oyabongz <skj8704@gmail.com>
2014-01-18 23:00:09 +0900

    insert src/

---
+++ src/com/mb/kids_mind/fragment/SketchMenu.java
@@ -0,0 +1,62 @@
+package com.mb.kids_mind.fragment;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v13.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.kids_mind.R;
+
+public class SketchMenu extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.menu_selector, null);
+		new AsyncTask<View, Void, View>() {
+
+			@Override
+			protected View doInBackground(View... params) {
+				while(!SketchMenu.this.isVisible()){
+					try {
+						Thread.sleep(50);
+					} catch (InterruptedException e) {}
+				}
+				return params[0];
+			}
+
+			@Override
+			protected void onPostExecute(View result) {
+				ViewPager pager = (ViewPager) result.findViewById(R.id.menu_pager);
+				pager.setAdapter(new ScreenSlidePagerAdapter(getFragmentManager()));
+				super.onPostExecute(result);
+			}
+			
+		}.execute(view);
+		
+		return view ;
+	}
+	
+	private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+		public ScreenSlidePagerAdapter(FragmentManager fm) {
+			super(fm);
+        }
+	
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+		@Override
+		public Fragment getItem(int position) {
+			SingleSketchMenu frag = new SingleSketchMenu();
+			frag.setPosition(position);
+			return frag;
+		}
+	}	
+}
